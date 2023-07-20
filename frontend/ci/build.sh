e9e3ea6a44564a371ed97529276e6b656a555fbf
gildedpleb <gildedpleb@protonmail.com>
2023-07-20 06:43:13 -0700

    Fix buikld script for sh

--- frontend/ci/build.sh
+++ frontend/ci/build.sh
@@ -3,10 +3,10 @@
 echo "...Building React App..."
 echo "STAGE: ${STAGE}"
 
-if [ "${STAGE}" == "dev" ]
+if [ "${STAGE}" = "dev" ]
 then
   echo "DEV URL: ${REACT_APP_API_URL_HTTP_DEV}"
-elif [ "${STAGE}" == "prod" ]
+elif [ "${STAGE}" = "prod" ]
 then
   echo "PROD URL: ${REACT_APP_API_URL_HTTP_PROD}"
 else
