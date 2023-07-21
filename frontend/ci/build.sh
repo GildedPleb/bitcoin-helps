fc4fdefe842fd674f0e068e2b0cd19a8e8bcfae4
gildedpleb <gildedpleb@protonmail.com>
2023-07-21 09:29:26 -0700

    Fix code gen staging

--- frontend/ci/build.sh
+++ frontend/ci/build.sh
@@ -3,17 +3,25 @@
 echo "...Building React App..."
 echo "STAGE: ${STAGE}"
 
+if [[ "$STAGE" == "prod" && -z "$GITHUB_ACTIONS" ]]
+then
+  echo "Prod deployments should only be executed in GitHub Actions."
+  exit 1
+fi
+
 if [ "${STAGE}" = "dev" ]
 then
   echo "DEV URL: ${REACT_APP_API_URL_HTTP_DEV}"
+  export SCHEMA=${REACT_APP_API_URL_HTTP_DEV}
 elif [ "${STAGE}" = "prod" ]
 then
   echo "PROD URL: ${REACT_APP_API_URL_HTTP_PROD}"
+  export SCHEMA=${REACT_APP_API_URL_HTTP_PROD}
 else
   echo "Invalid stage."
   exit 1
 fi
 
-npm run generate
+npx graphql-codegen
 
 npm run build
