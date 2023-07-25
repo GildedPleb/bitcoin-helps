09a0b05a71dd3c69ddaac54227771d5af77c934e
gildedpleb <gildedpleb@protonmail.com>
2023-07-25 10:36:07 -0700

    Remove 'React' import

--- frontend/src/app/components/buttons/back.tsx
+++ frontend/src/app/components/buttons/back.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import MinimalButton from "./styles/minimal";
 
 interface BackButtonProperties {
--- frontend/src/app/components/menu-bar/index.tsx
+++ frontend/src/app/components/menu-bar/index.tsx
@@ -1,5 +1,4 @@
 import styled from "@emotion/styled";
-import React from "react";
 
 import { type LeftToRightOrRightToLeft } from "../../../types";
 import BackButton from "../buttons/back";
