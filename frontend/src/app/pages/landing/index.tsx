fdd1881e05961096c05f4dc4967a2a650706de5b
gildedpleb <gildedpleb@protonmail.com>
2023-09-05 12:22:58 -0700

    remove useEffect

--- frontend/src/app/pages/landing/index.tsx
+++ frontend/src/app/pages/landing/index.tsx
@@ -1,6 +1,6 @@
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import {
