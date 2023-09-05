6419d385436ad60762f8673d174408719263c320
gildedpleb <gildedpleb@protonmail.com>
2023-09-05 11:53:07 -0700

    Fix up titling

--- frontend/src/app/components/text/title.tsx
+++ frontend/src/app/components/text/title.tsx
@@ -7,23 +7,32 @@ import { FADE_IN_OUT } from "../../../utilities/constants";
 
 const Title = styled.h1<{ direction: LeftToRightOrRightToLeft }>`
   animation: ${fadeIn} ${FADE_IN_OUT / 1000}s forwards;
-  font-size: clamp(1.75rem, 5.5vw, 3.75rem);
+  font-size: clamp(2.5rem, 5.5vw, 3.75rem);
   font-weight: bold;
   max-width: 650px;
-  min-height: 80px;
+  min-height: 120px;
   line-height: 1.5;
 
-  padding-right: clamp(50px, 30%, 250px);
+  padding-right: calc(20px + 32px + 50px);
   padding-left: 2rem;
   overflow: hidden;
+
+  word-break: break-word;
+  hyphens: auto;
+  overflow-wrap: break-word;
+  white-space: break-spaces;
 
   ${(properties) =>
     properties.direction === "rtl" &&
     css`
       text-align: right;
       padding-right: 2rem;
-      padding-left: clamp(50px, 30%, 250px);
+      padding-left: calc(20px + 32px + 50px);
     `}
+  @media (min-width: 600px) {
+    word-break: normal;
+    hyphens: none;
+  }
 `;
 
 export default Title;
